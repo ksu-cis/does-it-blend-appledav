1f761c24bb573e7ae2a7c74233501c46bbdd0656
David Apple <appledav@ksu.edu>
2020-02-05 14:15:32 -0600

    Lab demonstration

--- DoesItBlend/Program.cs
+++ DoesItBlend/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DoesItBlend
 {
@@ -6,7 +7,25 @@ namespace DoesItBlend
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello world");
+
+            var b = new Banana();
+            var s = new Strawberry();
+            var m = new Mango();
+            var c = new IceCubes();
+            var p = new CellPhone();
+
+            Console.WriteLine(b.Blend());
+            Console.WriteLine(s.Blend());
+            Console.WriteLine(m.Blend());
+
+            List<IBlendable> list = new List<IBlendable>() { b, s, m, c, p};
+
+            foreach (var f in list)
+            {
+                Console.WriteLine(f.GetType());
+                Console.WriteLine(f.Blend());
+            }
         }
     }
 }
